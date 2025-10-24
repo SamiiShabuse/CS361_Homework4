c2ee0dc5e1173c36ce28c66dd14977eb597671d4
Samii Shabuse <sshabuse@gmail.com>
2025-10-24 09:11:24 -0400

    race.cpp: create RaceState struct base

--- src/race.cpp
+++ src/race.cpp
@@ -1,1 +1,29 @@
-// initializing this file for race
+#include <vector>
+#include <thread>
+#include <mutex>
+#include <atomic>
+
+
+struct RaceState {
+    // movement and finish
+    std::vector<int> distance;          // size r, per race distance
+    std::vector<bool> finished;         // size r
+    std::vector<std::pair<int, std::thread::id>> finish_order; // size r, order of finishing (distance, thread id)
+
+
+    // completion tracking
+    int finished_count = 0;          // number of participants finished
+    int total_races = 0;              // total number of participants (threads)
+    std::atomic<bool> all_finished{false};  // atomic flag to indicate all have finished
+
+    // threadsafe dice queue
+    ThreadSafeQueue<int> dice_queue;  // queue of dice rolls
+
+    // For completion tracking
+    std::mutex completion_mtx;         // mutex for synchronizing access to completion tracking data
+
+    // For I/O synchronization
+    std::mutex io_mtx;                 // mutex for synchronizing access to shared data
+
+    std::mutex rank_mtx;              // mutex for synchronizing access to finish_order and finished_count
+};
