42770f8066012d691a1659ec339132d21028aba2
Samii Shabuse <sshabuse@gmail.com>
2025-10-24 09:01:15 -0400

    adding initial checkers

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,25 @@
+#include <iostream>
 
+int main (int argc, char** argv) {
+    // Check for exactly one argument
+    if (argc < 2 or argc > 2) {
+        std::cerr << "usage: " << argv[0] << " <number>\n";
+        return -1;
+    }
+    
+    // Check to make sure the argument is a number greater than 0
+    try {
+        int number = std::stoi(argv[1]);
+        if (number <= 0) {
+            std::cerr << "Error: " << argv[1] << " is not a positive number.\n";
+            return -1;
+        }
+    } catch (std::invalid_argument&) {
+        std::cerr << "Error: " << argv[1] << " is not a valid number.\n";
+        return -1;
+    } catch (std::out_of_range&) {
+        std::cerr << "Error: " << argv[1] << " is out of range.\n";
+        return -1;
+    }
 
-int main (int artgc, char** argv) {
-    return 0;
 }
